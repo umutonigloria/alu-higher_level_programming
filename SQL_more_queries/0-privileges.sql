f39b80b736559d0473442fa207229bb223f90a61
umutonigloria <g.umutoni2@alustudent.com>
2024-03-23 07:20:10 -0700

    privilages

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- list mysql privileges of two users.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users
+-- user_0d_1 and user_0d_2
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
