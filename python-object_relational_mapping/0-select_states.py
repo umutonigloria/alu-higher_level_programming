013e7b7e6efd46dbe792e4221b1c5c07cc7e0915
umutonigloria <g.umutoni2@alustudent.com>
2024-04-13 14:36:01 -0700

    mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,7 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
                          user=sys.argv[1], passwd=sys.argv[2])
