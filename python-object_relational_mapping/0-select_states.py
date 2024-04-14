5524f2f3d2f4c70a18881488e597545d95c8643c
umutonigloria <g.umutoni2@alustudent.com>
2024-04-14 11:15:57 -0700

    changed files

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
                          user=sys.argv[1], passwd=sys.argv[2])
     c = db.cursor()
-    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    for row in c.fetchall():
+    c.execute("SELECT * FROM states")
+    rows=c.fetchall()
+    for row in rows:
         print(row)
+    c.close()
+    db.close()
