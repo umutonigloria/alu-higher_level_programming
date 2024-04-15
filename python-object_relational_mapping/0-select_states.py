baf88ebe254adcd53cb81c252acc68ce2bd56c5d
umutonigloria <155954634+umutonigloria@users.noreply.github.com>
2024-04-15 08:56:09 +0200

    new changes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-''' Lists all states from the database hbtn0e0usa'''
+'''Lists all states from the database hbtn_0c_0_usa
+'''
 
+import MySQLdb
+import sys
 
-if _name == "__main":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cr = db.cursor()
-    cr.execute("SELECT * from states ORDER BY states.id")
-    states = cr.fetchall()
-    for state in states:
-        print(state)
-    cr.close()
-    db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
+                         user=sys.argv[1], passwd=sys.argv[2])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    for row in c.fetchall():
+        print(row)
